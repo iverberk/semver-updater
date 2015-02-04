4acfb6db8ef83df0791afefb91c92ee3e8c94eb5
Ivo Verberk <verberk@ict.eur.nl>
2015-02-04 17:10:29 +0100

    Update with patch

--- tag.sh
+++ tag.sh
@@ -5,7 +5,7 @@ VERSION=$(bin/semver get --no-build version.json)
 git tag --force -a $VERSION -m $VERSION
 git push --force origin $VERSION
 
-bin/semver increase version.json
+bin/semver increase --type patch version.json
 
 git add version.json
 git commit -m "Increase version"
